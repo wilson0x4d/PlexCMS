6d20248eeb61078fb2b64afc7989e63fda60ac9e
Shaun Wilson <mrshaunwilson@msn.com>
2013-05-11 23:30:07 -0700

    fix bad path to bootstrap

--- PreInstall.ps1
+++ PreInstall.ps1
@@ -5,7 +5,7 @@ Write-Host $("==== BEGIN PlexCMS Pre-Install Script");
 $removals = @{
 	"Controllers" = "HomeController.cs";
 	"Views" = "_ViewStart.cshtml";
-	"Layouts" = "_Classic,_Orange,_Purple";
+	"Layouts" = "_Classic.cshtml,_Orange.cshtml,_Purple.cshtml";
 	"App_Start" = "RouteConfig.cs,WebApiConfig.cs";
 	"Home" = "About.cshtml,Contact.cshtml,Index.cshtml";
 	"Areas" = "PlexAdmin";
@@ -14,7 +14,7 @@ $removals = @{
 function AttemptRemovals($folder) {
 	if (($folder.Kind -eq "{6BB5F8EF-4483-11D3-8BCF-00C04F8EC28C}") -or ($folder.Kind -eq "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}")) {
 		foreach ($item in $removals.GetEnumerator()) {
-			if ($folder.Name -eq $item.Key) {
+			if (($folder.Name -eq $item.Key) -or ($item.Key -eq "")) {
 				$filenames = $item.Value.Split(",");
 				foreach ($file in $folder.ProjectItems) {
 					foreach ($filename in $filenames) {
@@ -29,11 +29,13 @@ function AttemptRemovals($folder) {
 	}
 }
 
-foreach ($folder in $project.ProjectItems) {
-	AttemptRemovals($folder);
-	foreach ($subfolder in $folder.ProjectItems) {
-		AttemptRemovals($subfolder);
+function AttemptRemovalsRecursive($folder) {
+	foreach ($subFolder in $folder.ProjectItems) {
+		AttemptRemovals($subFolder);
+		AttemptRemovalsRecursive($subFolder);
 	}
 }
+
+AttemptRemovalsRecursive($project);
 
 Write-Host $("==== END PlexCMS Pre-Install Script");
